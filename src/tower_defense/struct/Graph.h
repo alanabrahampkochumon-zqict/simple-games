3063bc46ce54884b8aaa9e8eef324465ac46e39c
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-08-29 13:36:58 +0530

    feat: add impl for a-star

--- src/tower_defense/struct/Graph.h
+++ src/tower_defense/struct/Graph.h
@@ -13,6 +13,10 @@
 #include <queue>
 #include <unordered_map>
 #include <vector>
+#include <ranges>
+#include <math/SimpleMath.h>
+
+#include "Graph.h"
 
 namespace tower_defense::ds
 {
@@ -98,6 +102,8 @@ namespace tower_defense::ds
     //           -w3-[node4]
     struct WeightedGraphNode
     {
+        // Data
+        math::Vec2 position;
         // Outgoing edges
         std::vector<WeightedEdge*> edges;
     };
@@ -121,16 +127,16 @@ namespace tower_defense::ds
     using GBFSMap = std::unordered_map<const WeightedGraphNode*, GBFSScratch>;
 
 
-    inline float computeGBFSHeuristic(const WeightedGraphNode* start, const WeightedGraphNode* goal) noexcept
+    inline float computeHeuristic(const WeightedGraphNode* start, const WeightedGraphNode* goal) noexcept
     {
-        // TODO: Compute after adding heuristic data
-        return 1.0f;
+        // Manhattan distance (L2 Norm)
+        return start->position.dist(goal->position);
     }
 
     /// Perform a greedy breadth first search on the given weighted graph
     /// and return a `bool` indicating whether a path between @p start and @p goal exists.
     inline bool gbfs(const WeightedGraph& graph, const WeightedGraphNode* start, const WeightedGraphNode* goal,
-              GBFSMap& outMap) noexcept
+                     GBFSMap& outMap) noexcept
     {
         std::vector<const WeightedGraphNode*> openSet;
 
@@ -151,7 +157,7 @@ namespace tower_defense::ds
                     data.parentEdge = edge;
                     if (!data.inClosedSet)
                     {
-                        data.heuristic = computeGBFSHeuristic(edge->to, goal);
+                        data.heuristic = computeHeuristic(edge->to, goal);
                         data.inOpenSet = true;
                         openSet.emplace_back(edge->to);
                     }
@@ -180,5 +186,94 @@ namespace tower_defense::ds
         }
         while (current != goal);
         return current == goal;
+    }
+
+
+    ///+=+=+=+=+=+=+=+=
+    ///  A* Search
+    ///+=+=+=+=+=+=+=+=
+
+    /// Scratch structure used for A* search
+    struct AStarScratch
+    {
+        const WeightedEdge* parentEdge = nullptr;
+        bool inClosedSet = false;
+        bool inOpenSet = false;
+        float heuristic = 0.0f;
+        float actualFromStart = 0.0f; /// Cost from start to the current node
+    };
+
+    using AStarMap = std::unordered_map<const WeightedGraphNode*, AStarScratch>;
+
+    /// Perform an a-star search and returns whether a path exist from @p start to @p goal.
+    inline bool aStar(const WeightedGraph& graph, const WeightedGraphNode* start, const WeightedGraphNode* goal,
+                      AStarMap& outMap)
+    {
+        // Create an open set for keeping track of traversed element
+        std::vector<const WeightedGraphNode*> openSet;
+
+        // Make the start the current node and mark it as closed in outMap
+        const WeightedGraphNode* current = start;
+        outMap[current].inClosedSet = true;
+
+        // Compute the heuristic of the neighboring nodes and add them to the open set
+        // if they're not in the closed set(already traversed set) already
+        do
+        {
+            for (const WeightedEdge* edge : current->edges)
+            {
+                const WeightedGraphNode* neighbor = edge->to;
+                AStarScratch& data = outMap[neighbor];
+
+                if (!data.inClosedSet)
+                {
+                    // If the node is not the in open set then it must be the current node
+                    if (!data.inOpenSet)
+                    {
+                        data.parentEdge = edge;
+                        data.heuristic = computeHeuristic(neighbor, goal);
+                        // Compute the actual cost
+                        data.actualFromStart = outMap[current].actualFromStart + edge->weight;
+                        data.inOpenSet = true;
+                        openSet.emplace_back(neighbor);
+                    }
+                    else
+                    {
+                        // Compute the actual cost if this becomes the parent
+                        float newG = outMap[current].actualFromStart + edge->weight;
+                        if (newG < data.actualFromStart)
+                        {
+                            data.parentEdge = edge;
+                            data.actualFromStart = newG;
+                        }
+                    }
+                }
+
+                // Break the loop if the open set is empty
+                if (openSet.empty())
+                    break;
+
+                // Else make the node with the lowest heuristic + path cost the current node
+                const auto minNodeIterator = std::ranges::min_element(openSet.begin(), openSet.end(),
+                                                                      [&outMap](const WeightedGraphNode* a,
+                                                                      const WeightedGraphNode* b)
+                                                                      {
+                                                                          return outMap[a].heuristic + outMap[a].
+                                                                              actualFromStart < outMap[b].heuristic +
+                                                                              outMap[b].actualFromStart;
+                                                                      });
+
+                // Make the min node the current
+                current = *minNodeIterator;
+                // Delete it from open set
+                openSet.erase(minNodeIterator);
+                // Mark it as closed
+                outMap[current].inOpenSet = false;
+                outMap[current].inClosedSet = true;
+            }
+        }
+        while (current != goal);
+
+        return false;
     }
 } // namespace tower_defense::ds
