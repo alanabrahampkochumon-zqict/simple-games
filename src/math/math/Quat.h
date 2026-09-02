3113e39f7babb9c3e27e9e38535efb1a654bc11f
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-09-02 20:11:24 +0530

    refactor: update quaternion implementation to have a vector3 and a scalar instead of 4 scalars

--- src/math/math/Quat.h
+++ src/math/math/Quat.h
@@ -17,18 +17,36 @@ namespace math
 {
     struct Quat
     {
-        float x, y, z, s; /// x, y, z make up the vector part and s the scalar part.
+        Vec3 v;
+        float s; /// x, y, z make up the vector part and s the scalar part.
+
+        [[nodiscard]] explicit constexpr Quat(const Vec3& v, float s) noexcept;
+
+        [[nodiscard]] explicit constexpr Quat(float x, float y, float z, float s) noexcept;
+
 
         /// Get a rotation quaternion.
         ///
         /// @param angle The rotation angle in radians. Use @ref math::toRad() to convert from degrees to radians.
         /// @param axis  The axis of rotation. Must be a unit vector.
-        [[nodiscard]] static Quat makeRotation(const float angle, Vec3 axis) noexcept
-        {
-            const float s = std::sin(angle * 0.5f);
-            const float c = std::cos(angle * 0.5f);
-            axis *= s;
-            return Quat{ .x = axis.x, .y = axis.y, .z = axis.z, .s = c };
-        }
+        [[nodiscard]] static Quat makeRotation(float angle, const Vec3& axis) noexcept;
     };
+
+
+
+    //+=+=+=+=+=+=+=+=+=
+    //  IMPLEMENTATION
+    //+=+=+=+=+=+=+=+=+=
+
+    constexpr Quat::Quat(const Vec3& v, const float s) noexcept: v{ v }, s{ s } {}
+
+    constexpr Quat::Quat(const float x, const float y, const float z, const float s) noexcept: v{ x, y, z }, s{ s } {}
+
+    inline Quat Quat::makeRotation(const float angle, const Vec3& axis) noexcept
+    {
+        // [axis * sin(a/2), cos(a/2)]
+        const float s = std::sin(angle * 0.5f);
+        const float c = std::cos(angle * 0.5f);
+        return Quat{ .v = axis * s, .s = c };
+    }
 } // namespace math
