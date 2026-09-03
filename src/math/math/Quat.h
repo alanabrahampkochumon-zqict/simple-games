c9332b7748a5a37d904edb59af1a5921c46c33e9
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-09-03 10:29:38 +0530

    feat: add quaternion to matrix support

--- src/math/math/Quat.h
+++ src/math/math/Quat.h
@@ -9,6 +9,7 @@
  * @copyright Copyright (c) 2026 Alan Abraham P Kochumon
  */
 
+#include "mat/Mat4.h"
 #include "vec/Vec3.h"
 
 #include <cmath>
@@ -29,16 +30,24 @@ namespace math
         [[nodiscard]] explicit constexpr Quat(float x, float y, float z, float s) noexcept;
 
 
-        /// Get a rotation quaternion.
+        /// @brief Get a rotation quaternion.
         ///
         /// @param angle The rotation angle in radians. Use @ref math::toRad() to convert from degrees to radians.
         /// @param axis  The axis of rotation. Must be a unit vector.
         [[nodiscard]] static Quat makeRotation(float angle, const Vec3& axis) noexcept;
 
 
-        /// Get the Grassman product
+        /// @brief Perform quaternion multiplication
         /// @note Rotation is applied in a right to left order.
-        [[nodiscard]] constexpr Quat operator^(const Quat& other) const noexcept;
+        [[nodiscard]] constexpr Quat operator*(const Quat& other) const noexcept;
+
+        /// @brief Get the conjugate of this quaternion.
+        ///
+        /// @note For unit quaternion, this is the inverse.
+        [[nodiscard]] constexpr Quat conjugate() const noexcept;
+
+        /// @brief Get the matrix(rotation) from this quaternion.
+        [[nodiscard]] constexpr Mat4 toMat() const noexcept;
     };
 
 
@@ -51,14 +60,46 @@ namespace math
 
     constexpr Quat::Quat(const float x, const float y, const float z, const float s) noexcept: v{ x, y, z }, s{ s } {}
 
-
-    constexpr Quat Quat::operator^(const Quat& other) const noexcept
+    constexpr Quat Quat::operator*(const Quat& other) const noexcept
     {
-        // TODO: Add docs
-        // Vector Part (p^q)v =
+        // q1q2 = v1 x v2 + s1 * v2 + s2 * v1 - v1.v2
         auto v = v * other.s + other.v * s + v.cross(other.v);
         auto s = s * other.s - v.dot(other.v);
         return Quat{ v, s };
+    }
+
+    constexpr Quat Quat::conjugate() const noexcept { return Quat{ -v, s }; }
+
+    constexpr Mat4 Quat::toMat() const noexcept
+    {
+        // Here w implies scalar part s
+        const float x2 = x * x;
+        const float y2 = y * y;
+        const float z2 = z * z;
+        const float xy = v.x * v.y;
+        const float yz = v.y * v.z;
+        const float zx = v.z * v.x;
+
+        const float wx = s * v.x;
+        const float wy = s * v.y;
+        const float wz = s * v.z;
+
+        return Mat4{ 1 - 2 * (y2 + z2),
+                     2 * (xy - wz),
+                     2 * (zx + wy),
+                     0,
+                     2 * (xy + wz),
+                     1 - 2 * (x2 + z2),
+                     2 * (yz - wx),
+                     0,
+                     2 * (zx - wy),
+                     2 * (yz + wx),
+                     1 - 2 * (x2 + y2),
+                     0,
+                     0,
+                     0,
+                     0,
+                     1 };
     }
 
     inline Quat Quat::makeRotation(const float angle, const Vec3& axis) noexcept
--- src/math/math/vec/Vec3.h
+++ src/math/math/vec/Vec3.h
@@ -35,6 +35,9 @@ namespace math
         /// @brief Subtract a vector from this vector and return a new vector containing the vector difference.
         [[nodiscard]] constexpr Vec3 operator-(const Vec3& other) const noexcept;
 
+        /// @brief Negate the vector components
+        [[nodiscard]] constexpr Vec3 operator-() const noexcept;
+
         /// @brief Multiply this vector by a scalar and return a new scaled vector.
         [[nodiscard]] constexpr Vec3 operator*(float scalar) const noexcept;
 
@@ -91,6 +94,9 @@ namespace math
 
     constexpr Vec3 Vec3::operator-(const Vec3& other) const noexcept
     { return Vec3{ x - other.x, y - other.y, z - other.z }; }
+
+
+    constexpr Vec3 Vec3::operator-() const noexcept { return Vec3{ -x, -y, -z }; }
 
     constexpr Vec3 Vec3::operator*(const float scalar) const noexcept
     { return Vec3{ x * scalar, y * scalar, z * scalar }; }
